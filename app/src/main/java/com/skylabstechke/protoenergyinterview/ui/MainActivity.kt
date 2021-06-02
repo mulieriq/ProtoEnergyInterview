9136c40d857b859db0c80ca5e4aab83c7959e47a
EricRootLee <erickiokom@gmail.com>
2021-06-03 01:37:40 +0300

    small fix

--- app/src/main/java/com/skylabstechke/protoenergyinterview/ui/MainActivity.kt
+++ app/src/main/java/com/skylabstechke/protoenergyinterview/ui/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.skylabstechke.protoenergyinterview.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.skylabstechke.protoenergyinterview.R
 
 class MainActivity : AppCompatActivity() {
@@ -14,16 +15,27 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.title = "Payment Options"
 
 
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.filter_menu_canceled -> {
+                Toast.makeText(this, "Canceled", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.filter_menu_pending -> {
+                Toast.makeText(this, "Pending", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater:MenuInflater = menuInflater
-        inflater.inflate(R.menu.payment_options_menu,menu)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.payment_options_menu, menu)
         return true
     }
 }
