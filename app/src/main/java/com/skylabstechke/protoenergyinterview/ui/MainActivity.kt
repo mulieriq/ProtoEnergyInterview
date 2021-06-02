8b0c79bb6cbfde4dcaf2474159e11dab168a9014
EricRootLee <erickiokom@gmail.com>
2021-06-03 01:10:20 +0300

    Linked menu

--- app/src/main/java/com/skylabstechke/protoenergyinterview/ui/MainActivity.kt
+++ app/src/main/java/com/skylabstechke/protoenergyinterview/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package com.skylabstechke.protoenergyinterview.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.skylabstechke.protoenergyinterview.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.title = "Payment Options"
 
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater:MenuInflater = menuInflater
+        inflater.inflate(R.menu.payment_options_menu,menu)
+        return true
     }
 }
