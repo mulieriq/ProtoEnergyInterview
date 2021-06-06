71fc1e1a20928ae1019566d6fcfffd765f26ab9e
EricRootLee <erickiokom@gmail.com>
2021-06-06 21:49:49 +0300

    Mutable livedata

--- app/src/main/java/com/skylabstechke/protoenergyinterview/viewmodels/OrderViewModel.kt
+++ app/src/main/java/com/skylabstechke/protoenergyinterview/viewmodels/OrderViewModel.kt
@@ -3,7 +3,11 @@ package com.skylabstechke.protoenergyinterview.viewmodels
 import android.app.Application
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.skylabstechke.protoenergyinterview.models.MockData
+import com.skylabstechke.protoenergyinterview.utils.NetworkResult
 
 class OrderViewModel @ViewModelInject constructor(application: Application) :
     AndroidViewModel(application) {
+        var orderResponse : MutableLiveData<NetworkResult<MockData>> = MutableLiveData()
 }
