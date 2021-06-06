d4e248b50f1631754b977e336bdad838fbb74c72
EricRootLee <erickiokom@gmail.com>
2021-06-07 02:53:38 +0300

    Observing data change

--- app/src/main/java/com/skylabstechke/protoenergyinterview/ui/MainActivity.kt
+++ app/src/main/java/com/skylabstechke/protoenergyinterview/ui/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.skylabstechke.protoenergyinterview.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.skylabstechke.protoenergyinterview.R
+import com.skylabstechke.protoenergyinterview.adapters.OrderRecyclerViewAdapter
+import com.skylabstechke.protoenergyinterview.databinding.ActivityMainBinding
+import com.skylabstechke.protoenergyinterview.databinding.ActivityRowLayoutBinding
+import com.skylabstechke.protoenergyinterview.utils.NetworkResult
+import com.skylabstechke.protoenergyinterview.viewmodels.OrderViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+
+    private val mAdapter by lazy { OrderRecyclerViewAdapter() }
+    private lateinit var orderViewModel: OrderViewModel
+
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +37,9 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.apply {
             title = "Payment Options"
         }
+        orderViewModel = ViewModelProvider(this).get(OrderViewModel::class.java)
+        setUpRecyclerView()
+        requestApi()
     }
 
     override fun onDestroy() {
@@ -44,4 +66,38 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.payment_options_menu, menu)
         return true
     }
+   private fun setUpRecyclerView(){
+        binding.recyclerView.adapter = mAdapter
+        binding.recyclerView.layoutManager =LinearLayoutManager(this)
+        //showshimmereffect
+    }
+
+  private  fun requestApi(){
+      orderViewModel.getOrders()
+      orderViewModel.orderResponse.observe(this, Observer { response->
+          when (response) {
+              is NetworkResult.Success -> {
+                  Log.d("REQUEST DATA SUCCESS", "Requesting DATA SUCCESS")
+
+                  response.data?.let { mAdapter.setData(it) }
+              }
+              is NetworkResult.Error -> {
+
+                  Toast.makeText(
+                      this,
+                      response.message.toString(),
+                      Toast.LENGTH_SHORT
+                  ).show()
+              }
+              is NetworkResult.Loading -> {
+                  Toast.makeText(
+                      this,
+                      "Loading",
+                      Toast.LENGTH_SHORT
+                  ).show()
+              }
+          }
+
+      })
+  }
 }
