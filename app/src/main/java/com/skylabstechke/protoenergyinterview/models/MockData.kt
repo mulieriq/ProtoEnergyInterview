f7fa00e666ad9fd64e54ad81599f33886994cdf7
EricRootLee <erickiokom@gmail.com>
2021-06-06 20:26:44 +0300

    DifUtil

---
+++ app/src/main/java/com/skylabstechke/protoenergyinterview/models/MockData.kt
@@ -0,0 +1,3 @@
+package com.skylabstechke.protoenergyinterview.models
+
+data class MockData(private val orderId:String)
---
+++ app/src/main/java/com/skylabstechke/protoenergyinterview/utils/OrdersDefaultUtil.kt
@@ -0,0 +1,26 @@
+package com.skylabstechke.protoenergyinterview.utils
+
+import androidx.recyclerview.widget.DiffUtil
+import com.skylabstechke.protoenergyinterview.models.MockData
+
+class OrdersDefaultUtil(
+    private val oldOrders: List<MockData>,
+    private val newOrders: List<MockData>
+
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldOrders.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newOrders.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldOrders[oldItemPosition] === newOrders[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldOrders[oldItemPosition] == newOrders[newItemPosition]
+    }
+}
