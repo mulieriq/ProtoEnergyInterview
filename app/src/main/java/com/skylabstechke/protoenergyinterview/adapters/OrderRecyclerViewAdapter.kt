be10e8cf5801609f9ab1bf293ae986becc295a8b
EricRootLee <erickiokom@gmail.com>
2021-06-07 11:35:47 +0300

    TImezone

--- app/src/main/java/com/skylabstechke/protoenergyinterview/adapters/OrderRecyclerViewAdapter.kt
+++ app/src/main/java/com/skylabstechke/protoenergyinterview/adapters/OrderRecyclerViewAdapter.kt
@@ -1,12 +1,16 @@
 package com.skylabstechke.protoenergyinterview.adapters
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.skylabstechke.protoenergyinterview.databinding.ActivityRowLayoutBinding
 import com.skylabstechke.protoenergyinterview.models.OrdersModel
 import com.skylabstechke.protoenergyinterview.utils.OrdersDefaultUtil
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrderRecyclerViewAdapter : RecyclerView.Adapter<OrderRecyclerViewAdapter.MyViewHolder>() {
     private var orders = OrdersModel()
@@ -29,11 +33,21 @@ class OrderRecyclerViewAdapter : RecyclerView.Adapter<OrderRecyclerViewAdapter.M
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val dateFormat = SimpleDateFormat(
+            "yyyy-MM-dd'T'HH:mm:ss.SSSSSS",
+            Locale.getDefault()
+        )
+        //dateFormat.timeZone = TimeZone.getTimeZone("GMT")
+        val source = orders[position].dateCreated
+        val vdate = dateFormat.parse(source!!)
+
         holder.binding.customerName.text = orders[position].customerName
         holder.binding.deliveryPoint.text = orders[position].deliveryPointName
         holder.binding.status.text = orders[position].status
-        holder.binding.dateCreated.text = orders[position].dateCreated
+        holder.binding.dateCreated.text = vdate.toString()
 
     }
 
