cae428c4b51c6120d7d34142e5870158e800a2e6
EricRootLee <erickiokom@gmail.com>
2021-06-15 23:12:11 +0300

    net

--- app/src/main/java/com/skylabstechke/protoenergyinterview/utils/NetworkCheck.kt
+++ app/src/main/java/com/skylabstechke/protoenergyinterview/utils/NetworkCheck.kt
@@ -1,23 +1,26 @@
-//package com.skylabstechke.protoenergyinterview.utils
-//
-//import android.app.Application
-//import android.content.Context
-//import android.net.ConnectivityManager
-//import android.net.NetworkCapabilities
-//
-//class NetworkCheck {
-//    private val application: Application = Application()
-//    fun hasInternetConnection(): Boolean {
-//        val connectivityManager = application
-//            .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-//        val activeNetWork = connectivityManager.activeNetwork ?: return false
-//        val capabilities = connectivityManager.getNetworkCapabilities(activeNetWork) ?: return false
-//
-//        return when {
-//            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-//            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-//            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-//            else -> false
-//        }
-//    }
-//}
+package com.skylabstechke.protoenergyinterview.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
+
+@ActivityRetainedScoped
+abstract class NetworkCheck @Inject constructor(@ApplicationContext private val context: Context) {
+
+    fun hasInternetConnection(): Boolean {
+        val connectivityManager = context
+            .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetWork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetWork) ?: return false
+
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
+}
